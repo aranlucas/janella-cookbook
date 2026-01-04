8fc836618fa58ff169d41c5fab2720f2e86a1657
Lucas <aranlucas@gmail.com>
2026-01-03 20:07:58 -0800

    transpilepackages fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["jsdom"],
+  transpilePackages: ["jsdom"], // Add the name of the problematic module here
   experimental: {
     turbopackUseSystemTlsCerts: true,
   },
